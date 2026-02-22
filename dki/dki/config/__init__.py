aefef5f1b01e30bfeb83e53ec60162df4dfe6042
Lucas Ma <lucas_ma2025@126.com>
2026-02-23 01:42:13 +0800

    Delete dki directory

--- dki/dki/config/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Configuration module for DKI system."""
-
-from dki.config.config_loader import ConfigLoader
-
-__all__ = ["ConfigLoader"]
--- dki/web/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Web interface for DKI system."""
-
-from dki.web.app import create_app
-
-__all__ = ["create_app"]
